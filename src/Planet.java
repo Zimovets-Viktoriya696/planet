c27d69011022acdb82b69a54dfa1aff710f7198a
Zimovets-Viktoriya696 <zc.vika@gmail.com>
2016-06-19 21:46:39 +0300

    Initial commit

---
+++ src/Planet.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Виктория on 18.06.16.
+ */
+public class Planet {
+   private String name;
+   private String colour;
+   private int size;
+
+    public Planet ( final String name,final String colour,final int size){
+       this.name=name;
+       this.colour=colour;
+       this.size=size;
+    }
+}
